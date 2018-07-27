bf6bc286a7662c47a6d59120cba293a02d196a27
2636704902 <13311595201@163.com>
2018-07-27 18:04:41 +0800

    sha1

--- js/js.js
+++ js/js.js
@@ -97,13 +97,14 @@ $.ajax({
             var timestamp = Date.parse(new Date()),
                 nonceStr = randomString(20),
                 url = location.href;
-            var string1 = `noncestr=${nonceStr}&jsapi_ticket=${data}&timestamp=${timestamp}&url=${url}`
+            var string1 = `jsapi_ticket=${data}&noncestr=${nonceStr}&timestamp=${timestamp}&url=${url}`
+            // console.log(SHA1(string1))
             wx.config({
                 debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: 'wxe3c0e6248494fcc9', // 必填，公众号的唯一标识
                 timestamp, // 必填，生成签名的时间戳
                 nonceStr, // 必填，生成签名的随机串
-                signature: binb2hex(string1),// 必填，签名
+                signature: SHA1(string1),// 必填，签名
                 jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表
             });
         },
